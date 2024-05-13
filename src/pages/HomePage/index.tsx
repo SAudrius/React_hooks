5885050479f52386cdba00d33dc1c32d03c29c00
SAudrius <audrius@sabalys.com>
2024-05-13 23:03:48 +0300

    hook: useState

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+
 export const HomePage = () => {
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    setCount((prev) => prev + 1);
+  };
+  const handleMinus = () => {
+    setCount((prev) => prev - 1);
+  };
   return (
-    <div className='container'>
+    <div className='container '>
       <h2 className='text-4xl text-center text-white mt-12'>React hooks</h2>
+      <div className='mt-20 grid justify-center'>
+        <h3>Use State</h3>
+        <p className='text-2xl text-center'>Count: {count}</p>
+        <div className='flex gap-4 mt-4'>
+          <button
+            onClick={handleMinus}
+            className='bg-neutral-500 rounded-md px-6 py-2'
+          >
+            -1
+          </button>
+          <button
+            onClick={handleAdd}
+            className='bg-neutral-500 rounded-md px-6 py-2'
+          >
+            +1
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
